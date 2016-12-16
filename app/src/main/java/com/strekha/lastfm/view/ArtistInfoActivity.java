6f52b91854b42dda7d6288f0dc489ae656f31469
strekha <mr.choo96@gmail.com>
2016-12-16 17:00:13 +0300

    create artistInfoActivity

---
+++ app/src/main/java/com/strekha/lastfm/view/ArtistInfoActivity.java
@@ -0,0 +1,15 @@
+package com.strekha.lastfm.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.strekha.lastfm.R;
+
+public class ArtistInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artist_info);
+    }
+}
