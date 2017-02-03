b27570804f568805dea1ae18b87fac7d35e90d7d
AndrewDroid <po4ta4ka@gmail.com>
2017-02-03 19:19:59 +0300

    [code review]

--- app/src/main/java/com/strekha/lastfm/model/ArtistInfoModel.java
+++ app/src/main/java/com/strekha/lastfm/model/ArtistInfoModel.java
@@ -1,11 +1,13 @@
 package com.strekha.lastfm.model;
 
 
+import java.util.Optional;
+
+import com.strekha.lastfm.entity.info.Artist;
+import com.strekha.lastfm.entity.info.ArtistInfo;
 import com.strekha.lastfm.model.db.DatabaseHelper;
 import com.strekha.lastfm.model.deserialization.JsonParser;
 import com.strekha.lastfm.model.network.LastFM;
-import com.strekha.lastfm.entity.info.ArtistInfo;
-import com.strekha.lastfm.entity.info.Artist;
 
 import rx.Observable;
 
@@ -26,8 +28,14 @@ public class ArtistInfoModel {
         return Observable.fromCallable(() ->
         {
             String json = DatabaseHelper.getInstance().readJson(artist);
+            //TODO use ternary operator instead of if/else block. or Optional; see below...
+//            Optional.ofNullable(json).map(this::getArtist).orElse(null);
             if (json == null) return null;
             return JsonParser.parse(ArtistInfo.class, json).getArtist();
         });
     }
+
+//    private Artist getArtist(String j) {
+//        return JsonParser.parse(ArtistInfo.class, j).getArtist();
+//    }
 }
